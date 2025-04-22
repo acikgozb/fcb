336aa050c686e2d1825edda95df58dae1576ed37
acikgozb <acikgozb@proton.me>
2025-04-22 16:23:41 +0300

    feat: fcb: Read stdin for CODE
    The content of the code block can be passed to fcb via stdin as well as
    using the positional argument, like below:
    
    1 - `fcb 'println!("Hello world!");'`
    2 - `echo 'println!("Hello world!");' | fcb`
    3 - `fcb < code_block.txt`
    4 - `fcb <<< 'println!("Hello world!");'
    
    Therefore, stdin is read when the positional argument is not passed.
    
    Error messages are customized by wrapping `io::Error` with
    `FcbError::CannotReadStdin`.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/cli.rs
+++ src/cli.rs
@@ -15,8 +15,41 @@ struct Cli {
     code: Option<String>,
 }
 
-pub fn run() -> Result<(), io::Error> {
+#[derive(Debug)]
+pub enum FcbError {
+    CannotReadStdin(io::Error),
+}
+
+impl std::fmt::Display for FcbError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FcbError::CannotReadStdin(err) => {
+                write!(f, "{PROGRAM}: failed to read stdin: {}", err)
+            }
+        }
+    }
+}
+
+impl std::error::Error for FcbError {}
+
+pub fn run() -> Result<(), FcbError> {
     let cli = Cli::parse();
-    Ok(())
+    let code = match cli.code {
+        Some(code) => Ok(code),
+        None => read_from_stdin().map_err(FcbError::CannotReadStdin),
+    }?;
+
+}
+
+fn read_from_stdin() -> io::Result<String> {
+    let mut stdin = io::stdin();
+
+    if stdin.is_terminal() {
+        return Ok("".to_string());
+    }
+
+    let mut buf = String::new();
+    let _ = stdin.read_to_string(&mut buf)?;
+    Ok(buf)
 }
 
